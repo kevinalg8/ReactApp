10d9f6cc40f5a7e35e4342bfa3b31f36abea49d0
Kevin_A <kalondono35@soy.sena.edu.co>
2023-12-09 11:10:36 -0500

    VolviendoAprogramar

---
+++ projects/React-Project/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+
+//Los componentes siempre tienen que ser PascalCase para que react los interprete como tal 
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+<React.Fragment>
+<button>Hola m undito</button>
+<button>Hola m undito</button>
+</React.Fragment>
+
+)
