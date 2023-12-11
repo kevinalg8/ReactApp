2a890fea7ab67042c6ae33cd93b28ccdf3e43aea
Kevin_A <kalondono35@soy.sena.edu.co>
2023-12-10 22:50:47 -0500

    ReactPt2

--- projects/React-Project/src/components/App.jsx
+++ projects/React-Project/src/components/App.jsx
@@ -1,11 +1,21 @@
 import '../styles/app.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+/*
+Cuando pasamos un boolean con solo poner isFollowing react lo tomara como un true 
+pero si queremos que sea false le pasamos un isFollowing={false} es decir le decimos
+que esta vez si es falso
+*/
+
 export function App() {
+/*
+Podemos pasar una funcion como parametro es decir un crear una funcion la cual tiene como parametro
+en este caso la del TwitterFollowCard 
+*/
 return(
     <>
-    <TwitterFollowCard userName="Lilpeep" name="GOTH ANGEL SINGER"/>
+    <TwitterFollowCard isFollowing={false} userName="Lilpeep" name="GOTH ANGEL SINGER"/>
     <TwitterFollowCard userName="PostMalone" name="Post Malone"/>
-    <TwitterFollowCard userName="TrvisXX" name="Travis Scott"/>
+    <TwitterFollowCard isFollowing userName="TrvisXX" name="TRAVIS SCOTT"/>
     </>
 )}
--- projects/React-Project/src/components/TwitterFollowCard.jsx
+++ projects/React-Project/src/components/TwitterFollowCard.jsx
@@ -1,11 +1,16 @@
+//Todo lo que esta dentro del return son elementos 
+
 export function TwitterFollowCard({userName, name, isFollowing}) {
+    console.log(isFollowing);
+
+    //En la linea 10 estamos evaluando dicho elemento y a su vez renderizo
     return (
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img alt="Avatar" src={`https://unavatar.io/Twitter/${userName}`} className='tw-followCard-avatar' />
                 <div className='tw-followCard-info'>
                     <strong className='tw-followCard-infoUserName'>{name}</strong>
-                    <span>@{userName}</span>
+                    <span className="tw-followCard-infoUserName">@{userName}</span>
                 </div>
             </header>
             <aside>
@@ -14,3 +19,8 @@ export function TwitterFollowCard({userName, name, isFollowing}) {
         </article>
     )
 }
+/*
+Un componente es una factoria de elementos es decir es una funcion que al ejecutarla devuelve un elemento
+y el elemento es lo que factoriza
+LOS COMPONENTES CREAN ELEMENTOS Y LOS ELEMENTOS SON LOS QUE SE RENDERIZAN
+*/
