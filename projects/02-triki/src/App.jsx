f8108d7f957fb665889bd8a958fb23806556be38
Kevin_A <kalondono35@soy.sena.edu.co>
2023-12-26 22:47:32 -0500

    Turns

--- projects/02-triki/src/App.jsx
+++ projects/02-triki/src/App.jsx
@@ -1,15 +1,20 @@
   import { useState } from 'react'
 import './styles/index.css'
   
-  const turns = {
-    x: 'X',
-    O: 'O'
+  const TURNS = {
+    x: 'x',
+    O: 'o'
   }
 
   
-  const Square = ({children, updateBoard, i}) =>{
+  const Square = ({children, updateBoard, i,isSelected}) =>{
+    const className = `square ${isSelected ? 'is-selected' :''}`
+
+    const handleClick=()=>{
+      updateBoard(i)
+    }
     return(
-      <div className="square">
+      <div onClick={handleClick} className={className}>
         {children}
       </div>
     )
@@ -17,19 +22,36 @@ import './styles/index.css'
   
   function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn]= useState(TURNS.X)
+
+  const updateBoard = ()=>{
+    const newBoard= [... board]
+    newBoard[i]= turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn)
+
+  }
   return (
     <main className='board'>
       <h1>Triki</h1>
       <section className="game">
         {
-          board.map((_, i) =>{
+          board.map((_, index) =>{
             return(
-              <Square key={i} i={i}>
-                {i}
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {board[index]}
               </Square>
             )
           })
         }
+
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
 
       </section>
     </main>
