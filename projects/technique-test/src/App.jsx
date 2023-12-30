569c88aee970e2e61a18d00635e24cd8dcdb38b5
Kevin_A <kalondono35@soy.sena.edu.co>
2023-12-29 23:42:04 -0500

    errorConLasImagenes

--- projects/technique-test/src/App.jsx
+++ projects/technique-test/src/App.jsx
@@ -1,38 +1,50 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import './css/styles.css'
 
-const CAT_ENDPOINT_RANDOM_FACT ='https://catfact.ninja/fact'
+const CAT_ENDPOINT_RANDOM_FACT ='https://catfact.ninja/fact';
+const CAT_PREFIX_IMAGE_URL ='https://cataas.com';
 
 export function App() {
-    const [fact, setFact ] = useState('lorem ipsum cat fact')
 
+    const [fact, setFact ] = useState()
+    const [imageUrl, setImageUrl] = useState()
+
+    //Datasos de los michis (Efectos)
     useEffect(()=>{
         fetch(CAT_ENDPOINT_RANDOM_FACT)
         .then(res=> res.json())
         .then(data=>{
             const { fact } = data
-            setFact(fact)
-
+            setFact( fact )
             /*
             Crear un arrar dividiendo el texto por lo espacion luego
             decimos que escoja no la primer posicion sino de la primera
             a la 3 posicion y lo unimos de nuevo con el join en un string
             */
-           
-            const threeFirtsWords = fact.split(' ').slice(0,3).join(' ')
-            console.log(threeFirtsWords);
-            //API de imagenes random de michis
-            fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-            .then(res=>res.json())
-            .then(response=>{
-                console.log(response);
-            })
         })
     },[])
 
+    //Recuperar imagen esta es dependiente de los datos de los gatos
+    useEffect(()=> {
+        if (!fact) return
+        const threeFirtsWords = fact.split(' ', 3).join(' ')
+        console.log(threeFirtsWords)
+
+        //API de imagenes random de michis
+        //https://cataas.com/cat/says/${threeFirtsWords}?fontSize=50&fontColor=red
+        fetch(`https://cataas.com/cat/says/${threeFirtsWords}?fontSize=50&fontColor=red`)
+        .then(res=>res.json())
+        .then(response=>{
+            const { url } = response
+            setImageUrl(url)
+        })
+    },[fact])
+
     return(
         <main>
-            <h2>Hola esta es mi primer prueba con React</h2>
-            {fact &&<p>{fact}</p>}
+            <h2>API michis</h2>
+            {fact && <p>{fact}</p>}
+            {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`}/>}
         </main>
     )
     
